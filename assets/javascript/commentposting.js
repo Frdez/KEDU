98dec4cf48177fdd9d02319621ac50e73fbfadac
casperstaehr <32480275+Lillefyren@users.noreply.github.com>
2018-04-15 17:38:11 +0200

    question javascript
    Obviously cheating but still updates non the less

--- assets/javascript/commentposting.js
+++ assets/javascript/commentposting.js
@@ -1,17 +1,30 @@
+
+if(document.title == "Forum | KEDU")
+{
+  console.log("bubu");
+  var newPosttitle = document.querySelector("#newestQuestion > h4");
+  newPosttitle.innerHTML = localStorage.title;
+
+  var newPosttime = document.querySelector("#newestQuestion > p");
+  newPosttime.innerHTML = "Just Now";
+
+}
+
 function post()
 {
   //    <input type="text" name="title"> fra html
   //  var name = document.querySelector("input[name='title']");
   //  var age = document.querySelector("[name='age']");
 
-  var titleHolder = document.querySelector("input[name='title']");
-  var commentHolder = document.querySelector("input[name='commentfield']");
+  var titleHolder = document.querySelector("input[name='Title']");
+  var commentHolder = document.querySelector("[name='ask']");
+  var tagHolder = document.querySelector("input[name='tags']");
 
   localStorage.setItem("title", titleHolder.value);
-  localStorage.setItem("comment", commentHolder.value);
+  localStorage.setItem("question", commentHolder.value);
+  localStorage.setItem("tags", tagHolder.value);
 
-  console.log(localStorage.title);
-  //window.location.href = "forum.html";
+  window.location.href = "../forum.html";
 }
 
 function upvote()
